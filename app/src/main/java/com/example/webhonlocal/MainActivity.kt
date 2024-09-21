540cf547594dadf2083d833f202006b6f5dd83b3
Blumaie <ccoiffier33@gmail.com>
2024-09-21 11:30:05 +0200

    UX/UI add

--- app/src/main/java/com/example/webhonlocal/MainActivity.kt
+++ app/src/main/java/com/example/webhonlocal/MainActivity.kt
@@ -31,6 +31,21 @@ import androidx.compose.runtime.*
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.*
+import androidx.compose.ui.geometry.*
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.graphics.Shadow
 
 // Data classes
 data class MangaResponse(val data: List<Manga>)
@@ -92,7 +107,7 @@ fun Header() {
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .background(Color.DarkGray)
+            .background(Color(0xFF011724))
             .padding(8.dp)
     ) {
         Row(
@@ -100,7 +115,7 @@ fun Header() {
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             // Add your logo or text here
-            Text("Bibliothèque", color = Color.White, style = MaterialTheme.typography.headlineMedium)
+            Text("Bibliothèque", color = Color(0xFFD4F5F5), style = MaterialTheme.typography.headlineMedium)
             // Add any other elements (e.g. a search icon or menu)
         }
         Spacer(modifier = Modifier.height(4.dp))
@@ -108,33 +123,59 @@ fun Header() {
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceEvenly
         ) {
-            Text("Last update", color = Color.LightGray)
-            Text("Most follow", color = Color.LightGray)
-            Text("New manga", color = Color.LightGray)
+            Text("Last update", color = Color(0xFFD4F5F5))
+            Text("Most follow", color = Color(0xFFD4F5F5))
+            Text("New manga", color = Color(0xFFD4F5F5))
+        }
+        // Add a white divider here
+        Spacer(modifier = Modifier.height(8.dp)) // Add some space before the divider
+        Divider(
+            color = Color.White, // Set the divider color to white
+            thickness = 2.dp // Set the thickness of the divider
+        )
+        Spacer(modifier = Modifier.height(8.dp)) // Add some space after the divider
+    }
+}
+
+
+
+@Composable
+fun Footer(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color(0xFF011724))
+            .padding(8.dp)
+    ) {
+        // Ajouter une ligne blanche au-dessus des boutons du footer
+        Divider(
+            color = Color.White, // Couleur de la ligne blanche
+            thickness = 2.dp // Épaisseur de la ligne
+        )
+        Spacer(modifier = Modifier.height(8.dp)) // Ajouter un espace après la ligne
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            FooterButton(iconText = "Accueil") {
+                navController.navigate("grid") // Navigation vers la page de défilement des mangas
+            }
+            FooterButton(iconText = "Follow") { /* Handle Follow click */ }
+            FooterButton(iconText = "Search") { /* Handle Search click */ }
+            FooterButton(iconText = "Login") { /* Handle Login click */ }
         }
     }
 }
 
-@Composable
-fun Footer(navController: NavController) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(Color.DarkGray)
-            .padding(8.dp),
-        horizontalArrangement = Arrangement.SpaceEvenly
-    ) {
-        FooterButton(iconText = "Accueil", onClick = { /* Handle Accueil click */ })
-        FooterButton(iconText = "Follow", onClick = { /* Handle Follow click */ })
-        FooterButton(iconText = "Search", onClick = { /* Handle Search click */ })
-        FooterButton(iconText = "Login", onClick = { /* Handle Login click */ })
-    }
-}
+
 
 @Composable
 fun FooterButton(iconText: String, onClick: () -> Unit) {
     Column(
-        horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         // You can replace Text with Icon if you have appropriate icons
         Text(text = iconText, color = Color.White, style = MaterialTheme.typography.bodySmall)
@@ -148,6 +189,7 @@ fun FooterButton(iconText: String, onClick: () -> Unit) {
     }
 }
 
+
 // Rectangle Grid Composable
 @Composable
 fun RectangleGrid(navController: NavController, modifier: Modifier = Modifier) {
@@ -156,28 +198,56 @@ fun RectangleGrid(navController: NavController, modifier: Modifier = Modifier) {
 
     LazyVerticalGrid(
         columns = GridCells.Fixed(3),
-        contentPadding = PaddingValues(8.dp),
+        contentPadding = PaddingValues(4.dp),
         modifier = modifier.fillMaxSize()
     ) {
         items(mangaData.data) { manga ->
             Box(
                 modifier = Modifier
-                    .padding(4.dp)
-                    .height(100.dp)
+                    .padding(8.dp)
+                    .height(160.dp)
                     .fillMaxWidth()
                     .clickable {
                         navController.navigate("details/${manga.mal_id}")
                     }
             ) {
+                // Image
                 Image(
                     painter = rememberImagePainter(manga.images.jpg.image_url),
                     contentDescription = manga.title,
                     modifier = Modifier.fillMaxSize()
                 )
+
+                // Title at the bottom
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .align(Alignment.BottomCenter)
+                        .padding(7.dp)
+                ) {
+                    Text(
+                        text = manga.title,
+                        style = TextStyle(
+                            color = Color.White,
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Bold,
+                            textAlign = TextAlign.Center,
+                            shadow = Shadow(
+                                color = Color.Black,
+                                offset = Offset(1f, 1f),
+                                blurRadius = 6f
+                            )
+                        ),
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                }
             }
         }
     }
 }
+
+
 
 
 // Manga Details Screen Composable
@@ -188,13 +258,23 @@ fun MangaDetailsScreen(mangaId: Int, navController: NavController) {
     val manga = mangaData.data.find { it.mal_id == mangaId }
 
     manga?.let {
+        // Ajoute un état de scroll pour permettre le défilement
+        val scrollState = rememberScrollState()
+
         Column(
             modifier = Modifier
                 .padding(16.dp)
                 .fillMaxSize()
+                .verticalScroll(scrollState) // Appliquer le scroll vertical ici
         ) {
-            Text(text = it.title, style = MaterialTheme.typography.headlineLarge)
+            // Titre du manga en blanc
+            Text(
+                text = it.title,
+                style = MaterialTheme.typography.headlineLarge,
+                color = Color.White // Texte en blanc
+            )
             Spacer(modifier = Modifier.height(8.dp))
+            // Image du manga
             Image(
                 painter = rememberImagePainter(it.images.jpg.image_url),
                 contentDescription = it.title,
@@ -203,14 +283,22 @@ fun MangaDetailsScreen(mangaId: Int, navController: NavController) {
                     .height(200.dp)
             )
             Spacer(modifier = Modifier.height(8.dp))
-            Text(text = it.synopsis, style = MaterialTheme.typography.bodyLarge)
+            // Synopsis du manga en blanc
+            Text(
+                text = it.synopsis,
+                style = MaterialTheme.typography.bodyLarge,
+                color = Color.White // Texte en blanc
+            )
             Spacer(modifier = Modifier.height(16.dp))
+            // Bouton de retour
             Button(onClick = { navController.popBackStack() }) {
                 Text("Back")
             }
         }
     }
 }
+
+
 
 // Load JSON Function
 private fun loadJsonFromAssets(context: Context, fileName: String): MangaResponse {
