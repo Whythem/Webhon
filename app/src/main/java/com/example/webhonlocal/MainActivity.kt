b88552f3c044d66ea7bfc19644ec8e49d2699205
Jwarconsin <jeremie.warconsin@student.junia.com>
2024-09-21 14:00:47 +0200

    Api + button icon

--- app/src/main/java/com/example/webhonlocal/MainActivity.kt
+++ app/src/main/java/com/example/webhonlocal/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.webhonlocal
 
-import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,59 +11,21 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.*
-import coil.compose.rememberAsyncImagePainter
 import coil.compose.rememberImagePainter
-import com.example.webhonlocal.ui.theme.WebhonLocalTheme
 import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
-import java.io.InputStreamReader
-import androidx.compose.runtime.*
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.text.*
-import androidx.compose.ui.geometry.*
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.graphics.Shadow
-
-// Data classes
-data class MangaResponse(val data: List<Manga>)
-data class Manga(
-    val mal_id: Int,
-    val url: String,
-    val images: Images,
-    val titles: List<Title>,
-    val title: String,
-    val type: String,
-    val status: String,
-    val score: Double,
-    val synopsis: String
-)
-data class Images(val jpg: ImageUrls)
-data class ImageUrls(
-    val image_url: String
-)
-data class Title(val title: String)
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import androidx.compose.ui.res.painterResource
 
 // Main Activity
 class MainActivity : ComponentActivity() {
@@ -80,128 +41,41 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp() {
     val navController = rememberNavController()
+    var mangaList by remember { mutableStateOf<List<Manga>>(emptyList()) }
+
+    LaunchedEffect(Unit) {
+        mangaList = AnimeApiService.getTopManga()
+    }
+
     Scaffold(
         topBar = { Header() },
         bottomBar = { Footer(navController) }
     ) {
-        // Set the background color of the entire app to #011724
         Box(modifier = Modifier
             .fillMaxSize()
-            .background(Color(0xFF011724)) // Set the color here
+            .background(Color(0xFF011724))
             .padding(it)
         ) {
             NavHost(navController, startDestination = "grid") {
-                composable("grid") { RectangleGrid(navController) }
+                composable("grid") { RectangleGrid(navController, mangaList) }
                 composable("details/{mangaId}") { backStackEntry ->
                     val mangaId = backStackEntry.arguments?.getString("mangaId")?.toIntOrNull()
-                    mangaId?.let { MangaDetailsScreen(mangaId, navController) }
+                    mangaId?.let { MangaDetailsScreen(it, navController) }
                 }
             }
         }
     }
 }
 
-
-@Composable
-fun Header() {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(Color(0xFF011724))
-            .padding(8.dp)
-    ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            // Add your logo or text here
-            Text("Bibliothèque", color = Color(0xFFD4F5F5), style = MaterialTheme.typography.headlineMedium)
-            // Add any other elements (e.g. a search icon or menu)
-        }
-        Spacer(modifier = Modifier.height(4.dp))
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-            Text("Last update", color = Color(0xFFD4F5F5))
-            Text("Most follow", color = Color(0xFFD4F5F5))
-            Text("New manga", color = Color(0xFFD4F5F5))
-        }
-        // Add a white divider here
-        Spacer(modifier = Modifier.height(8.dp)) // Add some space before the divider
-        Divider(
-            color = Color.White, // Set the divider color to white
-            thickness = 2.dp // Set the thickness of the divider
-        )
-        Spacer(modifier = Modifier.height(8.dp)) // Add some space after the divider
-    }
-}
-
-
-
-@Composable
-fun Footer(navController: NavController) {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(Color(0xFF011724))
-            .padding(8.dp)
-    ) {
-        // Ajouter une ligne blanche au-dessus des boutons du footer
-        Divider(
-            color = Color.White, // Couleur de la ligne blanche
-            thickness = 2.dp // Épaisseur de la ligne
-        )
-        Spacer(modifier = Modifier.height(8.dp)) // Ajouter un espace après la ligne
-
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
-            horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-            FooterButton(iconText = "Accueil") {
-                navController.navigate("grid") // Navigation vers la page de défilement des mangas
-            }
-            FooterButton(iconText = "Follow") { /* Handle Follow click */ }
-            FooterButton(iconText = "Search") { /* Handle Search click */ }
-            FooterButton(iconText = "Login") { /* Handle Login click */ }
-        }
-    }
-}
-
-
-
-@Composable
-fun FooterButton(iconText: String, onClick: () -> Unit) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        // You can replace Text with Icon if you have appropriate icons
-        Text(text = iconText, color = Color.White, style = MaterialTheme.typography.bodySmall)
-        Spacer(modifier = Modifier.height(4.dp))
-        Box(
-            modifier = Modifier
-                .size(24.dp)
-                .background(Color.White.copy(alpha = 0.2f)) // Placeholder for an icon
-                .clickable { onClick() }
-        )
-    }
-}
-
-
 // Rectangle Grid Composable
 @Composable
-fun RectangleGrid(navController: NavController, modifier: Modifier = Modifier) {
-    val context = LocalContext.current
-    val mangaData by remember { mutableStateOf(loadJsonFromAssets(context, "data.json")) }
-
+fun RectangleGrid(navController: NavController, mangaList: List<Manga>, modifier: Modifier = Modifier) {
     LazyVerticalGrid(
         columns = GridCells.Fixed(3),
         contentPadding = PaddingValues(4.dp),
         modifier = modifier.fillMaxSize()
     ) {
-        items(mangaData.data) { manga ->
+        items(mangaList) { manga ->
             Box(
                 modifier = Modifier
                     .padding(8.dp)
@@ -211,14 +85,12 @@ fun RectangleGrid(navController: NavController, modifier: Modifier = Modifier) {
                         navController.navigate("details/${manga.mal_id}")
                     }
             ) {
-                // Image
                 Image(
                     painter = rememberImagePainter(manga.images.jpg.image_url),
                     contentDescription = manga.title,
                     modifier = Modifier.fillMaxSize()
                 )
 
-                // Title at the bottom
                 Box(
                     modifier = Modifier
                         .fillMaxWidth()
@@ -227,19 +99,9 @@ fun RectangleGrid(navController: NavController, modifier: Modifier = Modifier) {
                 ) {
                     Text(
                         text = manga.title,
-                        style = TextStyle(
-                            color = Color.White,
-                            fontSize = 16.sp,
-                            fontWeight = FontWeight.Bold,
-                            textAlign = TextAlign.Center,
-                            shadow = Shadow(
-                                color = Color.Black,
-                                offset = Offset(1f, 1f),
-                                blurRadius = 6f
-                            )
-                        ),
+                        overflow = TextOverflow.Ellipsis,
                         maxLines = 1,
-                        overflow = TextOverflow.Ellipsis
+                        color = Color.White
                     )
                 }
             }
@@ -247,69 +109,177 @@ fun RectangleGrid(navController: NavController, modifier: Modifier = Modifier) {
     }
 }
 
-
-
-
-// Manga Details Screen Composable
+// MangaDetailsScreen Composable
 @Composable
 fun MangaDetailsScreen(mangaId: Int, navController: NavController) {
-    val context = LocalContext.current
-    val mangaData = loadJsonFromAssets(context, "data.json")
-    val manga = mangaData.data.find { it.mal_id == mangaId }
+    var manga by remember { mutableStateOf<Manga?>(null) }
+    var isLoading by remember { mutableStateOf(true) }
+    var errorMessage by remember { mutableStateOf<String?>(null) }
 
-    manga?.let {
-        // Ajoute un état de scroll pour permettre le défilement
-        val scrollState = rememberScrollState()
+    LaunchedEffect(mangaId) {
+        try {
+            manga = AnimeApiService.getMangaDetails(mangaId)
+        } catch (e: Exception) {
+            errorMessage = "Erreur lors du chargement des détails."
+        } finally {
+            isLoading = false
+        }
+    }
 
-        Column(
-            modifier = Modifier
-                .padding(16.dp)
-                .fillMaxSize()
-                .verticalScroll(scrollState) // Appliquer le scroll vertical ici
-        ) {
-            // Titre du manga en blanc
-            Text(
-                text = it.title,
-                style = MaterialTheme.typography.headlineLarge,
-                color = Color.White // Texte en blanc
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            // Image du manga
-            Image(
-                painter = rememberImagePainter(it.images.jpg.image_url),
-                contentDescription = it.title,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(200.dp)
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            // Synopsis du manga en blanc
-            Text(
-                text = it.synopsis,
-                style = MaterialTheme.typography.bodyLarge,
-                color = Color.White // Texte en blanc
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            // Bouton de retour
-            Button(onClick = { navController.popBackStack() }) {
-                Text("Back")
+    if (isLoading) {
+        CircularProgressIndicator()
+    } else if (errorMessage != null) {
+        Text(text = errorMessage!!, color = Color.Red)
+    } else {
+        manga?.let {
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(text = it.title, color = Color.White)
+                Spacer(modifier = Modifier.height(8.dp))
+                Image(
+                    painter = rememberImagePainter(it.images.jpg.image_url),
+                    contentDescription = it.title,
+                    modifier = Modifier.fillMaxWidth().height(200.dp)
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(text = it.synopsis, color = Color.White)
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(onClick = { navController.popBackStack() }) {
+                    Text("Back")
+                }
+            }
+        } ?: run {
+            Text(text = "Manga non trouvé", color = Color.Red)
+        }
+    }
+}
+
+// API Service
+object AnimeApiService {
+    private val client = OkHttpClient()
+
+    suspend fun getTopManga(): List<Manga> {
+        val url = "https://api.jikan.moe/v4/top/manga?sfw=true"
+        val request = Request.Builder().url(url).build()
+
+        return withContext(Dispatchers.IO) {
+            client.newCall(request).execute().use { response ->
+                if (!response.isSuccessful) {
+                    println("Unexpected code $response")
+                    return@withContext emptyList()
+                }
+
+                val responseBody = response.body?.string()
+                responseBody?.let {
+                    val gson = Gson()
+                    val mangaResponse = gson.fromJson(it, MangaResponse::class.java)
+                    return@withContext mangaResponse.data
+                }
+                emptyList()
+            }
+        }
+    }
+
+    suspend fun getMangaDetails(mangaId: Int): Manga? {
+        val url = "https://api.jikan.moe/v4/manga/$mangaId"
+        val request = Request.Builder().url(url).build()
+
+        return withContext(Dispatchers.IO) {
+            client.newCall(request).execute().use { response ->
+                if (!response.isSuccessful) {
+                    println("Unexpected code $response")
+                    return@withContext null
+                }
+
+                val responseBody = response.body?.string()
+                responseBody?.let {
+                    val gson = Gson()
+                    val mangaResponse = gson.fromJson(it, MangaDetailsResponse::class.java)
+                    return@withContext mangaResponse.data
+                }
+                null
             }
         }
     }
 }
 
+// Data classes
+data class MangaResponse(val data: List<Manga>)
+data class MangaDetailsResponse(val data: Manga)
+data class Manga(
+    val mal_id: Int,
+    val url: String,
+    val images: Images,
+    val title: String,
+    val type: String,
+    val status: String,
+    val score: Double,
+    val synopsis: String
+)
+data class Images(val jpg: ImageUrls)
+data class ImageUrls(val image_url: String)
 
+// Header and Footer Composables
+@Composable
+fun Header() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color(0xFF011724))
+            .padding(8.dp)
+    ) {
+        Text("Bibliothèque", color = Color(0xFFD4F5F5), style = MaterialTheme.typography.headlineMedium)
+        Spacer(modifier = Modifier.height(4.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Text("Last update", color = Color(0xFFD4F5F5))
+            Text("Most follow", color = Color(0xFFD4F5F5))
+            Text("New manga", color = Color(0xFFD4F5F5))
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        Divider(color = Color.White, thickness = 2.dp)
+        Spacer(modifier = Modifier.height(8.dp))
+    }
+}
 
-// Load JSON Function
-private fun loadJsonFromAssets(context: Context, fileName: String): MangaResponse {
-    return try {
-        val inputStream = context.assets.open(fileName)
-        val reader = InputStreamReader(inputStream)
-        val gson = Gson()
-        val type = object : TypeToken<MangaResponse>() {}.type
-        gson.fromJson(reader, type)
-    } catch (e: Exception) {
-        e.printStackTrace()
-        MangaResponse(emptyList())
+@Composable
+fun Footer(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color(0xFF011724))
+            .padding(8.dp)
+    ) {
+        Divider(color = Color.White, thickness = 2.dp)
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            FooterButton(iconRes = R.drawable.ic_home, onClick = { navController.navigate("grid") })
+            FooterButton(iconRes = R.drawable.ic_follow, onClick = { /* Handle Follow click */ })
+            FooterButton(iconRes = R.drawable.ic_search, onClick = { /* Handle Search click */ })
+            FooterButton(iconRes = R.drawable.ic_login, onClick = { /* Handle Login click */ })
+        }
+    }
+}
+
+@Composable
+fun FooterButton(iconRes: Int, onClick: () -> Unit) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = rememberImagePainter(iconRes),
+            contentDescription = null,
+            modifier = Modifier
+                .size(24.dp)
+                .clickable { onClick() }
+        )
+        Spacer(modifier = Modifier.height(4.dp))
     }
 }
